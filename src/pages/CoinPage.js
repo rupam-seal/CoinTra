0d771794ac29c3fd2de05f3a857a5adc19317c19
rupam-seal <rupam.x.seal@gmail.com>
2023-01-02 00:07:29 +0530

    disable rule using // eslint-disable-next-line react-hooks/exhaustive-deps

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -24,6 +24,7 @@ const CoinPage = () => {
       .catch((error) => {
         console.log(error);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currency]);
   // ======== FETCH COIN ==========
 
