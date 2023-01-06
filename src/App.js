69df539499c5f1ca4ab1c592b998e840ddc90a84
rupam-seal <rupam.x.seal@gmail.com>
2023-01-06 11:14:00 +0530

    refactoring code

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// PAGES
+import HomePage from './pages/HomePage';
+import CoinPage from './pages/CoinPage';
+// COMPONENTS
+import Topbar from './components/Topbar';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/coins/:id" element={<CoinPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// PAGES
-import HomePage from './pages/HomePage';
-import CoinPage from './pages/CoinPage';
-// COMPONENTS
-import Topbar from './components/Topbar';
-
 // CSS
 // common css
 import './assets/common/Colors.css';
@@ -18,18 +12,13 @@ import './assets/homepage/Home.css';
 import './assets/homepage/Search.css';
 // single coins page css
 import './assets/coinpage/Coin.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import CryptoContext from './utils/CryptoContext';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <CryptoContext>
-    <BrowserRouter>
-      <Topbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} exact />
-        <Route path="/coins/:id" element={<CoinPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </CryptoContext>
 );
