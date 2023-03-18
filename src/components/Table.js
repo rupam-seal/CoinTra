e9d1ff6a5a7b479569c4854e22e4749585e03a45
rupam-seal <rupam.x.seal@gmail.com>
2023-03-18 10:31:23 +0530

    🐛fix: pagination item null

--- src/components/Table.js
+++ src/components/Table.js
@@ -50,20 +50,20 @@ const Table = memo(({ filterdCoins, symbol, page, setPage }) => {
   // ========= JSX =========
   return (
     <>
-      {mode && (
-        <ThemeProvider theme={darkTheme}>
-          {/* ========= TABLE HEADER START ========= */}
-          <div className="table__row table__row-header">
-            <span className="col col__1">#</span>
-            <span className="col col__2">Name</span>
-            <span className="col col__3">Price</span>
-            <span className="col col__4">Market Cap</span>
-            <span className="col col__5">Market Cap</span>
-            <span className="col col__6">24hr</span>
-            <span className="col col__7">7d</span>
-          </div>
-          {/* ========= TABLE HEADER END ========= */}
-          <div>{content}</div>
+      <ThemeProvider theme={darkTheme}>
+        {/* ========= TABLE HEADER START ========= */}
+        <div className="table__row table__row-header">
+          <span className="col col__1">#</span>
+          <span className="col col__2">Name</span>
+          <span className="col col__3">Price</span>
+          <span className="col col__4">Market Cap</span>
+          <span className="col col__5">Market Cap</span>
+          <span className="col col__6">24hr</span>
+          <span className="col col__7">7d</span>
+        </div>
+        {/* ========= TABLE HEADER END ========= */}
+        <div>{content}</div>
+        {mode && (
           <div className="pagination">
             <Pagination
               count={Number((filterdCoins?.length / 20).toFixed(0))}
@@ -75,8 +75,8 @@ const Table = memo(({ filterdCoins, symbol, page, setPage }) => {
               }}
             />
           </div>
-        </ThemeProvider>
-      )}
+        )}
+      </ThemeProvider>
     </>
   );
 });
